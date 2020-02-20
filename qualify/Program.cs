8a81e6a62dd16d7f564619106cfe06ee8c8c72b5
Clause828 <55769079+Clause828@users.noreply.github.com>
2020-02-20 10:22:52 -0800

    Pushing now
    Book and library class

---
+++ qualify/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hashCodeBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Library
+    {
+        
+    }
+    class Book
+    {
+        int ID;
+        int score;
+    }
+}
